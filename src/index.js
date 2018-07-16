72907950681e07f5e1e63a8ee1fb6630e51e86db
Xiaohan <xiaohan.zhu@outlook.com>
2018-07-16 18:09:54 +0800

    Hot loader frame

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './component/Relation.css';
+import { AppContainer } from 'react-hot-loader';
 import Home from './pages/Home.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <AppContainer>
+        <Home />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
+if(module.hot){
+    module.hot.accept('./pages/Home',()=>{
+        const NewHome = require('./pages/Home').default;
+        ReactDOM.render(
+            <AppContainer>
+                <NewHome />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
