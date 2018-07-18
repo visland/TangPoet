424ee303d9931e1351ac1580bce4dacf4f1b8be6
DataJournalism-CUC <datajournalism_CUC@126.com>
2018-07-18 11:52:50 +0800

    修改布局样式

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Relation from '../component/Relation';
 import CiCloud from '../component/CiCloud';
 import LeadIn from '../component/LeadIn'
 import PartOne from '../component/Part'
+import './style/style.less'
 
 export default class Home extends Component {
   componentDidMount(){
@@ -13,46 +14,9 @@ export default class Home extends Component {
       background: `url(${require("../component/img/background.png")})`
     }
 
-    //shows coordiante
-    // function mousePosition(ev){
-    //     if(ev.pageX || ev.pageY){
-    //         return {x:ev.pageX, y:ev.pageY};
-    //     }
-    //     return {
-    //         x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-    //         y:ev.clientY + document.body.scrollTop - document.body.clientTop
-    //     };
-    // }
-
-    // function mouseMove(ev){
-    //     ev = ev || window.event;
-    //     var mousePos = mousePosition(ev);
-    //     document.getElementById('xxx').value = mousePos.x;
-    //     document.getElementById('yyy').value = mousePos.y;
-    // }
-    // document.onmousemove = mouseMove;
-    // function mousePosition(ev){
-    //     if(ev.pageX || ev.pageY){
-    //         return {x:ev.pageX, y:ev.pageY};
-    //     }
-    //     return {
-    //         x:ev.clientX + document.body.scrollLeft - document.body.clientLeft,
-    //         y:ev.clientY + document.body.scrollTop - document.body.clientTop
-    //     };
-    // }
-     
-    // function mouseMove(ev){
-    //     ev = ev || window.event;
-    //     var mousePos = mousePosition(ev);
-    //     document.getElementById('xxx').value = mousePos.x;
-    //     document.getElementById('yyy').value = mousePos.y;
-    // }
-    // document.onmousemove = mouseMove;
-
     return (
       <div style = { bg } >
        <div id="title-title" style={{
-            position:'relative',
             top:'10px',
             height:"1080px",
             width:"1920px"
@@ -62,7 +26,6 @@ export default class Home extends Component {
         <PartOne />
 
         <div id="ci-cloud" style={{
-          position: 'relative',
           paddingTop: 100,
         }}>
           <CiCloud />
@@ -70,7 +33,6 @@ export default class Home extends Component {
         
 
         <div id="relation" style={{
-            position:'relative',
             paddingTop: 100,
           }}>
           <Relation />
