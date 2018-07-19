62256393ef5f15130719c1ef22282ca9252b427a
DataJournalism-CUC <datajournalism_CUC@126.com>
2018-07-19 18:40:25 +0800

    修改tooltip一个bug

--- src/component/ChartPoetry/Tooltip.js
+++ src/component/ChartPoetry/Tooltip.js
@@ -6,48 +6,29 @@ export default class Tooltip extends React.Component{
     render(){
         const { tooltip, hideToolTip } = this.props;
         let data = tooltip.data;
-        // let position = tooltip.pos;
-        
-
-        let visibility = 'hidden';
         let x = 0;
         let y = 0;
-        let width = 150;
-        let height = 150;
-        // let x = position.x;
-        // let y = position.y;
 
         if (tooltip.display === true) {
             let position = tooltip.pos;
             x = position.x;
             y = position.y;
-            visibility = 'visible';
-
-        } else {
-            visibility = 'hidden';
-
-        }
+        } 
         const style = {
             position: "absolute",
-            // top: y - height / 2, 
-            top: y - height / 2, 
-            left: x - width ,
-            // left: x - width / 2,
+            top: y, 
+            left: x,
+            visibility: tooltip.display ? "visible" : "hidden"
         }
         return(
             <div 
-                width={width} height={height} 
-                visibility={visibility}
-                
                 onMouseOut={hideToolTip}  
                 style={style}
-                className="tooltip"
-                      
+                className="tooltip"                
             >
                 <p className="poetry-name">{data.name}</p>
                 <p className="poetry-info">{data.info}</p>
                 <p className="poetry-value">诗作数量：{data.value}</p>
-
             </div>
         )
     }
